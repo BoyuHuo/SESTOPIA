11504db41b572a6f6961862f3b966644f1452b98
BoyuHuo <boyu.huo.china@gmail.com>
2020-07-23 22:36:15 -0400

    small commit

--- sestopia/routes/skills.js
+++ sestopia/routes/skills.js
@@ -1,25 +1,29 @@
-var  skillService = require("../service/skillService");
+var skillService = require("../service/skillService");
 var express = require('express');
 var router = express.Router();
 
 /* GET skills. */
 router.get('/', (req, res, next) => {
 
-   skillService.getAllSkills().then((data)=>{
-    res.send(data);
-  },(err)=>{
-    res.status(501).send("Can't get skills because : "+err.stack);
-  })
+    skillService.getAllSkills().then((data) => {
+        res.send(data);
+    }, (err) => {
+        res.status(501).send("Can't get skills because : " + err.stack);
+    })
 });
 
 /* GET skills. */
 router.get('/search', (req, res, next) => {
-   skillService.getSkillsByName("%"+req.query.name+"%").then((data)=>{
-    res.send(data);
-  },(err)=>{
-    res.status(501).send("Can't get skills because : "+err.stack);
-  })
+    skillService.getSkillsByName("%" + req.query.name + "%").then((data) => {
+        res.send(data);
+    }, (err) => {
+        res.status(501).send("Can't get skills because : " + err.stack);
+    })
 });
+
+//TODO Micheal
+/* Add a new skill */
+
 
 
 
