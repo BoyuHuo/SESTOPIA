60f61a03e7d43166a2664498004f52ed6e6c98ab
michaelhanna <michael.baligh@gmail.com>
2020-07-25 19:12:16 -0400

    Add test cases

--- sestopia/test/skillTest.js
+++ sestopia/test/skillTest.js
@@ -1,15 +1,13 @@
-var memberService = require("../service/memberService");
+var skillService = require("../service/skillService");
 var assert = require('assert');
 
-
 describe('Member', function() {
-    describe('get members', function() {
+    describe('get skillsç', function() {
         it('should return -1 when the value is not present', function() {
-            memberService.getAllMembers(function(data) {
-                assert.equal(data.length, 4);
+            skillService.getAllSkills(function(data) {
+                assert.equal(data.length, 11);
             }, function(err) {
             })
-
         });
     });
 
