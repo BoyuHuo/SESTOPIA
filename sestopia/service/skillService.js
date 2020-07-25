59fdbbbbaa96a046f49b28abe07d2752749d810a
michaelhanna <michael.baligh@gmail.com>
2020-07-25 13:25:43 -0400

    add skill service

--- sestopia/service/skillService.js
+++ sestopia/service/skillService.js
@@ -26,4 +26,16 @@ let getSkillsByName = function(name){
 }
 
 
-module.exports =  { getAllSkills,  getSkillsByName}; 
+let addSkill = function(classification,prerequisites,knowledge_area,rationale,roles_for_skill,related_activities,real_world_scenario,role_of_academia,tools,self_assessment,reference) {
+    return new Promise(function(resolve, reject) {
+        mysql.query('INSERT INTO skills (classification,prerequisites,knowledge_area,rationale,roles_for_skill,related_activities,real_world_scenario,role_of_academia,tools,self_assessment,reference) VALUES (' + mysql.pool.escape(classification) + ',' + mysql.pool.escape(prerequisites) + ',' + mysql.pool.escape(knowledge_area) + mysql.pool.escape(rationale) +mysql.pool.escape(roles_for_skill) +mysql.pool.escape(related_activities) +mysql.pool.escape(real_world_scenario) +mysql.pool.escape(role_of_academia) +mysql.pool.escape(tools) +mysql.pool.escape(self_assessment) +mysql.pool.escape(reference) +')',function(err, data, field) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+
+module.exports =  { getAllSkills,  getSkillsByName,addSkill}; 
