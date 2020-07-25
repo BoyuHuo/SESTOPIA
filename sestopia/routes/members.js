5aa11fbb632884a9a41427ac0fd6593bcf7fe7fa
BoyuHuo <boyu.huo.china@gmail.com>
2020-07-25 12:39:28 -0400

    issue fixed

--- sestopia/routes/members.js
+++ sestopia/routes/members.js
@@ -16,8 +16,7 @@ router.get('/', (req, res, next) => {
 /* Add a new memnber */
 
 router.post('/addmember', (req, res, next) => {
-
-    memberService.addMember(req.body.name,req.body.sno,req.body.email).then((data) => {
+    memberService.addMember(req.body.name, req.body.sno, req.body.email).then((data) => {
         res.send(data);
     }, (err) => {
         res.status(501).send("Can't get members because : " + err.stack);
