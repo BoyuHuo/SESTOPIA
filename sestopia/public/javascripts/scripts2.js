3db5bf5006bb48222d28bfc51770c36c6552ecf9
advanceP <shahryar.haghighi.fard@gmail.com>
2020-08-06 16:00:21 -0400

    members page header finished

--- sestopia/public/javascripts/scripts2.js
+++ sestopia/public/javascripts/scripts2.js
@@ -30,8 +30,7 @@ getSkills(getQueryVariable("name"));
 //Get skills by search content
 function getSkills(keyworkds) {
     request('/api/skills/search?name=' + keyworkds, 'GET', null).then(res => {
-        console.log(res);
-        // updateData(res[0]);
+        updateData(res[0]);
     }, err => {
         alert(res.MSG);
     })
@@ -40,28 +39,28 @@ function getSkills(keyworkds) {
 //Update Content
 function updateData(data) {
 
-    console.log("helooo:" +  data);
+    console.log(data);
 
-    // $("#studentName").append(data.student_name)
+    $("#studentName").append(data.student_name)
 
-    // var skillData = [
-    //     { skillPosition: "1 skill out of 12", name: "Name of Skill", skill: data.name },
-    //     { skillPosition: "2 skill out of 12", name: "Classification of Skill", skill: data.classification },
-    //     { skillPosition: "3 skill out of 12", name: "Prerequisites for Skill", skill: data.prerequisites },
-    //     { skillPosition: "4 skill out of 12", name: "Software Engineering Knowledge Area(s)", skill: data.knowledge_area },
-    //     { skillPosition: "5 skill out of 12", name: "Rationale for Skill", skill: data.rationale },
-    //     { skillPosition: "6 skill out of 12", name: "Roles for Skill", skill: data.roles_for_skill },
-    //     { skillPosition: "7 skill out of 12", name: "Related Activities", skill: data.related_activities },
-    //     { skillPosition: "8 skill out of 12", name: "Real-World Example/Scenario", skill: data.real_world_scenario },
-    //     { skillPosition: "9 skill out of 12", name: "Role of Academia or Industry in Cultivating the Skill", skill: data.role_of_academia },
-    //     { skillPosition: "10 skill out of 12", name: "Tools", skill: data.tools },
-    //     { skillPosition: "11 skill out of 12", name: "Skill Self-Assessment", skill: data.self_assessment },
-    //     { skillPosition: "12 skill out of 12", name: "References", skill: data.reference },
-    // ];
+    var skillData = [
+        { skillPosition: "1 skill out of 12", name: "Name of Skill", skill: data.name },
+        { skillPosition: "2 skill out of 12", name: "Classification of Skill", skill: data.classification },
+        { skillPosition: "3 skill out of 12", name: "Prerequisites for Skill", skill: data.prerequisites },
+        { skillPosition: "4 skill out of 12", name: "Software Engineering Knowledge Area(s)", skill: data.knowledge_area },
+        { skillPosition: "5 skill out of 12", name: "Rationale for Skill", skill: data.rationale },
+        { skillPosition: "6 skill out of 12", name: "Roles for Skill", skill: data.roles_for_skill },
+        { skillPosition: "7 skill out of 12", name: "Related Activities", skill: data.related_activities },
+        { skillPosition: "8 skill out of 12", name: "Real-World Example/Scenario", skill: data.real_world_scenario },
+        { skillPosition: "9 skill out of 12", name: "Role of Academia or Industry in Cultivating the Skill", skill: data.role_of_academia },
+        { skillPosition: "10 skill out of 12", name: "Tools", skill: data.tools },
+        { skillPosition: "11 skill out of 12", name: "Skill Self-Assessment", skill: data.self_assessment },
+        { skillPosition: "12 skill out of 12", name: "References", skill: data.reference },
+    ];
 
-    // skillData.forEach(item => {
-    //     $("#secondSliderContent").append("<li class=\"splide__slide skillItem\" >" + "<h1 class=\"skillName\">" + item.name + "</h1>" + "<h2 class=\"skillPosition\" >" + item.skillPosition + "</h2>" + "<p class=\"skillContent\">" + item.skill + "</p>" + "</li>");
-    // })
+    skillData.forEach(item => {
+        $("#secondSliderContent").append("<li class=\"splide__slide skillItem\" >" + "<h1 class=\"skillName\">" + item.name + "</h1>" + "<h2 class=\"skillPosition\" >" + item.skillPosition + "</h2>" + "<p class=\"skillContent\">" + item.skill + "</p>" + "</li>");
+    })
 
     secondarySlider.mount();
 
