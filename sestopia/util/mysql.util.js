f1ae31a15d954c66e4d1369a8989f1bfb4b33b75
BoyuHuo <boyu.huo.china@gmail.com>
2020-08-08 13:29:03 -0400

    fix style, fix search, add lint

--- sestopia/util/mysql.util.js
+++ sestopia/util/mysql.util.js
@@ -1,39 +1,40 @@
-const config = require("./mysql.config.js") 
+const config = require("./mysql.config.js")
 const mysql = require("mysql")
 
 //Config
 const pool = mysql.createPool({
     connectionLimit: 50,
-    host     :  config["host"],
-    user     : config["user"],
-    password : config["password"],
-    database : config["database"],
-     multipleStatements: true
-});    
+    host: config["host"],
+    user: config["user"],
+    password: config["password"],
+    database: config["database"],
+    multipleStatements: true
+});
 
-let query = function (sql, values) {
+let query = function(sql, values) {
 
-  return new Promise((resolve, reject) => {
-    pool.getConnection(function (err, connection) {
-      if (err) {
-        reject(err)
-      } else {
-        connection.query(sql, values, (err, rows) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(rows)
-          }
-  
-          connection.release()
+    return new Promise((resolve, reject) => {
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                reject(err)
+            } else {
+                connection.query(sql, values, (err, rows) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(rows)
+                    }
+
+                    connection.release()
+                })
+            }
         })
-      }
     })
-  })
 }
 
 
 
-module.exports =  {
-  query,pool
+module.exports = {
+    query,
+    pool
 }
