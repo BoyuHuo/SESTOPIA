af70c037c1a1f10593022f76bbf3fca479b117ee
advanceP <shahryar.haghighi.fard@gmail.com>
2020-08-11 22:42:20 -0400

    js files uglified and refactored

--- sestopia/public/javascripts/scripts3.js
+++ sestopia/public/javascripts/scripts3.js
@@ -1,11 +1,1 @@
-
-getAllMembers();
-
-//Get all members data
-function getAllMembers() {
-    request('/api/skills', 'GET', null).then(res => {
-        updateData(res);
-    }, err => {
-        alert(res.MSG);
-    });
-}
+function getAllMembers(){request("/api/skills","GET",null).then(e=>{updateData(e)},e=>{alert(res.MSG)})}getAllMembers();
